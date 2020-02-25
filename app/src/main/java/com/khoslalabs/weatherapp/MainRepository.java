f9be3cc6e9b49ae3c79a7b22e91bd4b45b399c9c
raushan <raushan.singh@hummingwave.com>
2020-02-25 23:23:19 +0530

    lifecycle dependencies

--- app/src/main/java/com/khoslalabs/weatherapp/MainRepository.java
+++ app/src/main/java/com/khoslalabs/weatherapp/MainRepository.java
@@ -2,14 +2,25 @@ package com.khoslalabs.weatherapp;
 
 import android.app.Application;
 
+import androidx.lifecycle.MutableLiveData;
+
 import com.khoslalabs.weatherapp.models.WeatherModel;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainRepository {
-    private ArrayList<Movie> movies=new ArrayList<>();
-    private MutableLiveData<List<WeatherModel>> mutableLiveData=new MutableLiveData<>();
+    private ArrayList<WeatherModel> weatherModels=new ArrayList<>();
+    private MutableLiveData<WeatherModel> mutableLiveData=new MutableLiveData<>();
     private Application application;
+
+    public MainRepository(Application application) {
+        this.application = application;
+    }
+
+    public MutableLiveData<WeatherModel> getMutableLiveData(){
+
+    }
+
 
 }
--- app/src/main/java/com/khoslalabs/weatherapp/viewModel/MainActivityViewModel.java
+++ app/src/main/java/com/khoslalabs/weatherapp/viewModel/MainActivityViewModel.java
@@ -1,6 +1,16 @@
 package com.khoslalabs.weatherapp.viewModel;
 
+import android.app.Application;
+
 import androidx.lifecycle.AndroidViewModel;
 
+import com.khoslalabs.weatherapp.MainRepository;
+
 public class MainActivityViewModel extends AndroidViewModel {
+    private MainRepository mainRepository;
+
+    public MainActivityViewModel(@NonNull Application application) {
+        super(application);
+        mainRepository=new MainRepository(application);
+    }
 }
